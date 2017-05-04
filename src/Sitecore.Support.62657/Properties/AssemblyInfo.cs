cc2790d7c816d558489174830a608fc0d52f84db
DK\AMCH <amch@sitecore.net>
2017-05-04 17:27:05 +0300

    commit patch

---
+++ src/Sitecore.Support.62657/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.62657")]
+[assembly: AssemblyProduct("Sitecore.Support.62657")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.62657/Social/Client/Mvc/Pipelines/Initialize/RegisterSocialArea.cs
@@ -0,0 +1,34 @@
+﻿using Sitecore.Pipelines;
+using Sitecore.Social.Client.Mvc.Areas.Social;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Sitecore.Support.Social.Client.Mvc.Pipelines.Initialize
+{
+  public class RegisterSocialArea
+  {
+    public virtual void Process(PipelineArgs args)
+    {
+      List<RouteBase> source = RouteTable.Routes.Where<RouteBase>(delegate (RouteBase route) {
+        Route route2 = route as Route;
+        return ((((route2 != null) && (route2.Defaults != null)) && route2.Defaults.ContainsKey("area")) && (route2.Defaults["area"].ToString() == "Social"));
+      }).ToList<RouteBase>();
+      if (!source.Any<RouteBase>())
+      {
+        new SocialAreaRegistration().RegisterArea(new AreaRegistrationContext("Social", RouteTable.Routes));
+      }
+      else
+      {
+        foreach (Route route in source)
+        {
+          if (route != null)
+          {
+            route.DataTokens["UseNamespaceFallback"] = true;
+          }
+        }
+      }
+    }
+  }
+}
